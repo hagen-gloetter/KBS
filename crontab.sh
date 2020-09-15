cd44cf15abe0a2c7f47216db1ebc11df59030c3a
hagen-gloetter <38193521+hagen-gloetter@users.noreply.github.com>
2020-09-16 01:35:26 +0200

    txt fixes

--- crontab.sh
+++ crontab.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 /usr/bin/python /home/pi/kbs/display/bootscreen.py
 sleep 5
+# twice because my nano sometimes misses the first command
 /usr/bin/python /home/pi/kbs/display/bootscreen.py
 sleep 5
 /usr/bin/python3 /home/pi/kbs/display/socket_server.py >> /home/pi/kbs/display/logfile.log
